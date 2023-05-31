0415f741b9233da99400047879173934dd69acd7
Benas Montrimas <benasm123@googlemail.com>
2023-05-31 17:22:57 +0100

    Entry added to application

---
+++ ProjectPearl/source/Entry.cpp
@@ -0,0 +1,26 @@
+#include <memory>
+#include "Core/Logger.h"
+#include "Render/Generic/Renderer.h"
+#include "Render/Vulkan/Renderer/VulkanRenderer2D.h"
+
+#ifdef __cplusplus
+extern "C"
+#endif
+
+int main(int argc, char* argv[])
+{
+	spdlog::set_level(spdlog::level::trace);
+
+	pearl::Window window("Pearl Engine", 1400, 1400);
+	// const std::unique_ptr<Renderer> renderer = std::make_unique<VulkanRenderer2D>(window);
+	auto renderer = VulkanRenderer2D(window);
+
+	bool running = true;
+	while (running)
+	{
+		running = window.Update();
+		renderer.Update();
+	}
+
+	return 0;
+}
