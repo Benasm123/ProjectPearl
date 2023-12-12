19c0386287e7adc966e765a018abfacbc6ef2f95
Benas Montrimas <74568812+Benasm123@users.noreply.github.com>
2023-12-12 11:14:59 +0000

    Delete PearlTesting directory

--- PearlTesting/Tests/Parts/Utils.cpp
+++
@@ -1,9 +0,0 @@
-// ReSharper disable All
-#include "pch.h"
-#include "../../PearlRenderer/source/PearlCore.h"
-
-TEST(Utils, ShaderRead)
-{
-	std::vector<char> code = utils::ReadShaderCode("Basic.vert.spv");
-	ASSERT_FALSE(code.empty());
-}
